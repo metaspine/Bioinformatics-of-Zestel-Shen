5c58e27c4c4a40e8d4417231662673cbabe9d81d
Zestel Shen <144090108+metaspine@users.noreply.github.com>
2024-01-03 15:18:21 +0800

    Update 0_Global_setting.R

--- Single cell/0_Global_setting.R
+++ Single cell/0_Global_setting.R
@@ -1,7 +1,9 @@
 # Part0：Global setting
-# To be modified：dir;
+# To be modified：dir; workers; maxSize
 rm(list=ls()); # Optional, remove all objects in the current environment when start a new project
 dir = "path/to/dir"
+workers = 12
+maxSize = 300000 * 1024^2
 Sys.setenv(LANGUAGE = "en"); # set the environment variable LANGUAGE to English
 options(stringsAsFactors = FALSE); # not convert strings to factors
 version # displays the version of the R software
@@ -11,5 +13,5 @@ getwd() # get working directory
 Sys.Date() # get the present time
 library(future) # to create a multisession job 
 # too many cores and too large RAM will cause "fork: retry: No child processes"
-plan("multisession", workers = 12)
-options(future.globals.maxSize = 300000 * 1024^2)
+plan("multisession", workers = workers)
+options(future.globals.maxSize = maxSize)
